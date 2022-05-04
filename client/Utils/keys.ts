9524457e9b252f20ba73c0de2cf5959b08d6b57c
Sudarshan Bashyal <50352683+sudarshanbashyal@users.noreply.github.com>
2022-05-04 21:38:54 +0545

    Delete keys.ts

--- client/Utils/keys.ts
+++
@@ -1,5 +0,0 @@
-export const MAPBOX_ACCESS_TOKEN =
-	"pk.eyJ1IjoiY2Fyc29udHVybmVyIiwiYSI6ImNrOW8zYmdibjA4MW8zaG84MnIyZ2x6bDEifQ.vpUo0q0bgiMCGSYSRoIYLQ";
-
-export const MAPBOX_LINK =
-	"https://api.mapbox.com/directions/v5/mapbox/driving/";
